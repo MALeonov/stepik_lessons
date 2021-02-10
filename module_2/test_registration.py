fc0509bb156c3c52ca122d9d5e2d586d7816fb2b
Maksim <m.a.leonov@yandex.ru>
2021-02-10 13:55:33 +0300

    final change test_registration.py

--- module_2/test_registration.py
+++ module_2/test_registration.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from sys import argv
-import time
 
 script_name, link = argv
 
@@ -9,20 +8,16 @@ try:
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element_by_tag_name("input")
+    input1 = browser.find_element_by_xpath("//input[@placeholder='Input your first name']")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_class_name("form-control.second")
+    input2 = browser.find_element_by_xpath("//input[@placeholder='Input your last name']")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("form-control.third")
+    input3 = browser.find_element_by_xpath("//input[@placeholder='Input your email']")
     input3.send_keys("mail@mail.com")
 
     # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
-
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
 
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element_by_tag_name("h1")
@@ -31,9 +26,6 @@ try:
 
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
-
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
 
 finally:
 
