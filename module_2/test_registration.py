00573172d10bae136aa3d3061ff848f0d71fcc9c
Maksim <m.a.leonov@yandex.ru>
2021-02-10 13:37:06 +0300

    change test_registration.py

--- module_2/test_registration.py
+++ module_2/test_registration.py
@@ -9,7 +9,12 @@ try:
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    ...
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_class_name("form-control.second")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name("form-control.third")
+    input3.send_keys("mail@mail.com")
 
     # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
@@ -27,8 +32,10 @@ try:
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
 
-finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
+
+finally:
+
     # закрываем браузер после всех манипуляций
     browser.quit()
