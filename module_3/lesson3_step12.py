ac5b471d97aaf0d6ba400db9d69e30ff566b80e1
Maksim <m.a.leonov@yandex.ru>
2021-02-17 16:28:02 +0300

    add lesson3_step7 - 12

--- module_3/lesson3_step12.py
+++ module_3/lesson3_step12.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
 
-link = "http://suninjuly.github.io/get_attribute.html"
+link = "http://suninjuly.github.io/selects2.html"
 
 try:
     browser = webdriver.Chrome()
@@ -10,10 +10,16 @@ try:
 
     num1 = browser.find_element_by_id("num1")
     num2 = browser.find_element_by_id("num2")
+    sum = str(int(num1.text) + int(num2.text))
 
     select = Select(browser.find_element_by_id("dropdown"))
-    select.select_by_visible_text(num1.text + num2.text)
+    select.select_by_visible_text(sum)
 
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+    time.sleep(30)
 
 finally:
+
     browser.quit()
