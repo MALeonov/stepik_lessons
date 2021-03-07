3318d33b62fd5a44103bf0c7dfadf8d81c418ca9
Maksim <m.a.leonov@yandex.ru>
2021-03-07 19:57:23 +0300

    edit part_1 hometask

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -3,4 +3,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_rus_text_of_button(browser):
     browser.get(link)
     add_button = browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert add_button.text == "Добавить в корзину" , "Wrong text of add-button, or not on RUS language!"
+    assert add_button.text in "Добавить в корзину Añadir al carrito Ajouter au panier Add to basket" , \
+        "Wrong text of add-button, or not on 'ru', 'en-GB', 'es', 'fr' language!"
